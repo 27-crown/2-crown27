60f01e00e8df41ccf2e3f6c8385588519f42f8af
27-crown <18660956227@163.com>
2026-08-28 02:58:00 +0800

    Add files via upload

---
+++ assets/cities-data.js
@@ -0,0 +1,304 @@
+window.TRAVEL_DATA = {
+  "welcomeTitle": "欢迎来到我的旅游剧本",
+  "welcomeSubtitle": "中国旅游地图",
+  "noPhotoCities": [
+    [
+      "广州",
+      "guangzhou"
+    ],
+    [
+      "杭州",
+      "hangzhou"
+    ],
+    [
+      "安吉",
+      "anji"
+    ],
+    [
+      "黄山",
+      "huangshan"
+    ],
+    [
+      "阜阳",
+      "fuyang"
+    ]
+  ],
+  "cities": [
+    {
+      "name": "山东临沂",
+      "short": "临沂",
+      "lng": 118.35,
+      "lat": 35.05,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/山东临沂/山东临沂_01.jpg",
+        "assets/photos/山东临沂/山东临沂_02.jpg",
+        "assets/photos/山东临沂/山东临沂_03.jpg",
+        "assets/photos/山东临沂/山东临沂_04.jpg",
+        "assets/photos/山东临沂/山东临沂_05.jpg",
+        "assets/photos/山东临沂/山东临沂_06.jpg",
+        "assets/photos/山东临沂/山东临沂_07.jpg",
+        "assets/photos/山东临沂/山东临沂_08.jpg",
+        "assets/photos/山东临沂/山东临沂_09.jpg",
+        "assets/photos/山东临沂/山东临沂_10.jpg",
+        "assets/photos/山东临沂/山东临沂_11.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "上海",
+      "short": "上海",
+      "lng": 121.47,
+      "lat": 31.23,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/上海/上海_01.jpg",
+        "assets/photos/上海/上海_02.jpg",
+        "assets/photos/上海/上海_03.jpg",
+        "assets/photos/上海/上海_04.jpg",
+        "assets/photos/上海/上海_05.jpg",
+        "assets/photos/上海/上海_06.jpg",
+        "assets/photos/上海/上海_07.jpg",
+        "assets/photos/上海/上海_08.jpg",
+        "assets/photos/上海/上海_09.jpg",
+        "assets/photos/上海/上海_10.jpg",
+        "assets/photos/上海/上海_11.jpg",
+        "assets/photos/上海/上海_12.jpg",
+        "assets/photos/上海/上海_13.jpg",
+        "assets/photos/上海/上海_14.jpg",
+        "assets/photos/上海/上海_15.jpg",
+        "assets/photos/上海/上海_16.jpg",
+        "assets/photos/上海/上海_17.jpg",
+        "assets/photos/上海/上海_18.jpg",
+        "assets/photos/上海/上海_19.jpg",
+        "assets/photos/上海/上海_20.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "新疆",
+      "short": "新疆",
+      "lng": 87.62,
+      "lat": 43.83,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/新疆/新疆_01.jpg",
+        "assets/photos/新疆/新疆_02.jpg",
+        "assets/photos/新疆/新疆_03.jpg",
+        "assets/photos/新疆/新疆_04.jpg",
+        "assets/photos/新疆/新疆_05.jpg",
+        "assets/photos/新疆/新疆_06.jpg",
+        "assets/photos/新疆/新疆_07.jpg",
+        "assets/photos/新疆/新疆_08.jpg",
+        "assets/photos/新疆/新疆_09.jpg",
+        "assets/photos/新疆/新疆_10.jpg",
+        "assets/photos/新疆/新疆_11.jpg",
+        "assets/photos/新疆/新疆_12.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "山东威海",
+      "short": "威海",
+      "lng": 122.12,
+      "lat": 37.51,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/山东威海/山东威海_01.jpg",
+        "assets/photos/山东威海/山东威海_02.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "河南郑州",
+      "short": "郑州",
+      "lng": 113.65,
+      "lat": 34.76,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/河南郑州/河南郑州_01.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "陕西西安",
+      "short": "西安",
+      "lng": 108.94,
+      "lat": 34.34,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/陕西西安/陕西西安_01.jpg",
+        "assets/photos/陕西西安/陕西西安_02.jpg",
+        "assets/photos/陕西西安/陕西西安_03.jpg",
+        "assets/photos/陕西西安/陕西西安_04.jpg",
+        "assets/photos/陕西西安/陕西西安_05.jpg",
+        "assets/photos/陕西西安/陕西西安_06.jpg",
+        "assets/photos/陕西西安/陕西西安_07.jpg",
+        "assets/photos/陕西西安/陕西西安_08.jpg",
+        "assets/photos/陕西西安/陕西西安_09.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "四川成都",
+      "short": "成都",
+      "lng": 104.07,
+      "lat": 30.57,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/四川成都/四川成都_01.jpg",
+        "assets/photos/四川成都/四川成都_02.jpg",
+        "assets/photos/四川成都/四川成都_03.jpg",
+        "assets/photos/四川成都/四川成都_04.jpg",
+        "assets/photos/四川成都/四川成都_05.jpg",
+        "assets/photos/四川成都/四川成都_06.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "四川德阳",
+      "short": "德阳",
+      "lng": 104.4,
+      "lat": 31.13,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/四川德阳/四川德阳_01.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "重庆",
+      "short": "重庆",
+      "lng": 106.55,
+      "lat": 29.56,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/重庆/重庆_01.jpg",
+        "assets/photos/重庆/重庆_02.jpg",
+        "assets/photos/重庆/重庆_03.jpg",
+        "assets/photos/重庆/重庆_04.jpg",
+        "assets/photos/重庆/重庆_05.jpg",
+        "assets/photos/重庆/重庆_06.jpg",
+        "assets/photos/重庆/重庆_07.jpg",
+        "assets/photos/重庆/重庆_08.jpg",
+        "assets/photos/重庆/重庆_09.jpg",
+        "assets/photos/重庆/重庆_10.jpg",
+        "assets/photos/重庆/重庆_11.jpg",
+        "assets/photos/重庆/重庆_12.jpg",
+        "assets/photos/重庆/重庆_13.jpg",
+        "assets/photos/重庆/重庆_14.jpg",
+        "assets/photos/重庆/重庆_15.jpg",
+        "assets/photos/重庆/重庆_16.jpg",
+        "assets/photos/重庆/重庆_17.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "湖南长沙",
+      "short": "长沙",
+      "lng": 112.94,
+      "lat": 28.23,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/湖南长沙/湖南长沙_01.jpg",
+        "assets/photos/湖南长沙/湖南长沙_02.jpg",
+        "assets/photos/湖南长沙/湖南长沙_03.jpg",
+        "assets/photos/湖南长沙/湖南长沙_04.jpg",
+        "assets/photos/湖南长沙/湖南长沙_05.jpg",
+        "assets/photos/湖南长沙/湖南长沙_06.jpg",
+        "assets/photos/湖南长沙/湖南长沙_07.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "江苏苏州",
+      "short": "苏州",
+      "lng": 120.62,
+      "lat": 31.3,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/江苏苏州/江苏苏州_01.jpg",
+        "assets/photos/江苏苏州/江苏苏州_02.jpg",
+        "assets/photos/江苏苏州/江苏苏州_03.jpg",
+        "assets/photos/江苏苏州/江苏苏州_04.jpg",
+        "assets/photos/江苏苏州/江苏苏州_05.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "湖北武汉",
+      "short": "武汉",
+      "lng": 114.31,
+      "lat": 30.52,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/湖北武汉/湖北武汉_01.jpg",
+        "assets/photos/湖北武汉/湖北武汉_02.jpg",
+        "assets/photos/湖北武汉/湖北武汉_03.jpg",
+        "assets/photos/湖北武汉/湖北武汉_04.jpg",
+        "assets/photos/湖北武汉/湖北武汉_05.jpg",
+        "assets/photos/湖北武汉/湖北武汉_06.jpg",
+        "assets/photos/湖北武汉/湖北武汉_07.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "北京",
+      "short": "北京",
+      "lng": 116.41,
+      "lat": 39.9,
+      "hasPhotos": true,
+      "photos": [
+        "assets/photos/北京/北京_01.jpg",
+        "assets/photos/北京/北京_02.jpg",
+        "assets/photos/北京/北京_03.jpg"
+      ],
+      "concept": null
+    },
+    {
+      "name": "广州",
+      "short": "广州",
+      "lng": 113.26,
+      "lat": 23.13,
+      "hasPhotos": false,
+      "photos": [],
+      "concept": null
+    },
+    {
+      "name": "杭州",
+      "short": "杭州",
+      "lng": 120.15,
+      "lat": 30.29,
+      "hasPhotos": false,
+      "photos": [],
+      "concept": null
+    },
+    {
+      "name": "安吉",
+      "short": "安吉",
+      "lng": 119.68,
+      "lat": 30.64,
+      "hasPhotos": false,
+      "photos": [],
+      "concept": null
+    },
+    {
+      "name": "黄山",
+      "short": "黄山",
+      "lng": 118.34,
+      "lat": 29.71,
+      "hasPhotos": false,
+      "photos": [],
+      "concept": null
+    },
+    {
+      "name": "阜阳",
+      "short": "阜阳",
+      "lng": 115.81,
+      "lat": 32.89,
+      "hasPhotos": false,
+      "photos": [],
+      "concept": null
+    }
+  ]
+};
