844ae82df968ee0ff51b7c85403207e6bd31f127
27-crown <18660956227@163.com>
2026-08-28 03:18:17 +0800

    Update cities-data.js

--- assets/cities-data.js
+++ assets/cities-data.js
@@ -31,17 +31,17 @@ window.TRAVEL_DATA = {
       "lat": 35.05,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/山东临沂/山东临沂_01.jpg",
-        "assets/photos/山东临沂/山东临沂_02.jpg",
-        "assets/photos/山东临沂/山东临沂_03.jpg",
-        "assets/photos/山东临沂/山东临沂_04.jpg",
-        "assets/photos/山东临沂/山东临沂_05.jpg",
-        "assets/photos/山东临沂/山东临沂_06.jpg",
-        "assets/photos/山东临沂/山东临沂_07.jpg",
-        "assets/photos/山东临沂/山东临沂_08.jpg",
-        "assets/photos/山东临沂/山东临沂_09.jpg",
-        "assets/photos/山东临沂/山东临沂_10.jpg",
-        "assets/photos/山东临沂/山东临沂_11.jpg"
+        "assets/山东临沂/山东临沂_01.jpg",
+        "assets/山东临沂/山东临沂_02.jpg",
+        "assets/山东临沂/山东临沂_03.jpg",
+        "assets/山东临沂/山东临沂_04.jpg",
+        "assets/山东临沂/山东临沂_05.jpg",
+        "assets/山东临沂/山东临沂_06.jpg",
+        "assets/山东临沂/山东临沂_07.jpg",
+        "assets/山东临沂/山东临沂_08.jpg",
+        "assets/山东临沂/山东临沂_09.jpg",
+        "assets/山东临沂/山东临沂_10.jpg",
+        "assets/山东临沂/山东临沂_11.jpg"
       ],
       "concept": null
     },
@@ -52,26 +52,26 @@ window.TRAVEL_DATA = {
       "lat": 31.23,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/上海/上海_01.jpg",
-        "assets/photos/上海/上海_02.jpg",
-        "assets/photos/上海/上海_03.jpg",
-        "assets/photos/上海/上海_04.jpg",
-        "assets/photos/上海/上海_05.jpg",
-        "assets/photos/上海/上海_06.jpg",
-        "assets/photos/上海/上海_07.jpg",
-        "assets/photos/上海/上海_08.jpg",
-        "assets/photos/上海/上海_09.jpg",
-        "assets/photos/上海/上海_10.jpg",
-        "assets/photos/上海/上海_11.jpg",
-        "assets/photos/上海/上海_12.jpg",
-        "assets/photos/上海/上海_13.jpg",
-        "assets/photos/上海/上海_14.jpg",
-        "assets/photos/上海/上海_15.jpg",
-        "assets/photos/上海/上海_16.jpg",
-        "assets/photos/上海/上海_17.jpg",
-        "assets/photos/上海/上海_18.jpg",
-        "assets/photos/上海/上海_19.jpg",
-        "assets/photos/上海/上海_20.jpg"
+        "assets/上海/上海_01.jpg",
+        "assets/上海/上海_02.jpg",
+        "assets/上海/上海_03.jpg",
+        "assets/上海/上海_04.jpg",
+        "assets/上海/上海_05.jpg",
+        "assets/上海/上海_06.jpg",
+        "assets/上海/上海_07.jpg",
+        "assets/上海/上海_08.jpg",
+        "assets/上海/上海_09.jpg",
+        "assets/上海/上海_10.jpg",
+        "assets/上海/上海_11.jpg",
+        "assets/上海/上海_12.jpg",
+        "assets/上海/上海_13.jpg",
+        "assets/上海/上海_14.jpg",
+        "assets/上海/上海_15.jpg",
+        "assets/上海/上海_16.jpg",
+        "assets/上海/上海_17.jpg",
+        "assets/上海/上海_18.jpg",
+        "assets/上海/上海_19.jpg",
+        "assets/上海/上海_20.jpg"
       ],
       "concept": null
     },
@@ -82,18 +82,18 @@ window.TRAVEL_DATA = {
       "lat": 43.83,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/新疆/新疆_01.jpg",
-        "assets/photos/新疆/新疆_02.jpg",
-        "assets/photos/新疆/新疆_03.jpg",
-        "assets/photos/新疆/新疆_04.jpg",
-        "assets/photos/新疆/新疆_05.jpg",
-        "assets/photos/新疆/新疆_06.jpg",
-        "assets/photos/新疆/新疆_07.jpg",
-        "assets/photos/新疆/新疆_08.jpg",
-        "assets/photos/新疆/新疆_09.jpg",
-        "assets/photos/新疆/新疆_10.jpg",
-        "assets/photos/新疆/新疆_11.jpg",
-        "assets/photos/新疆/新疆_12.jpg"
+        "assets/新疆/新疆_01.jpg",
+        "assets/新疆/新疆_02.jpg",
+        "assets/新疆/新疆_03.jpg",
+        "assets/新疆/新疆_04.jpg",
+        "assets/新疆/新疆_05.jpg",
+        "assets/新疆/新疆_06.jpg",
+        "assets/新疆/新疆_07.jpg",
+        "assets/新疆/新疆_08.jpg",
+        "assets/新疆/新疆_09.jpg",
+        "assets/新疆/新疆_10.jpg",
+        "assets/新疆/新疆_11.jpg",
+        "assets/新疆/新疆_12.jpg"
       ],
       "concept": null
     },
@@ -104,8 +104,8 @@ window.TRAVEL_DATA = {
       "lat": 37.51,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/山东威海/山东威海_01.jpg",
-        "assets/photos/山东威海/山东威海_02.jpg"
+        "assets/山东威海/山东威海_01.jpg",
+        "assets/山东威海/山东威海_02.jpg"
       ],
       "concept": null
     },
@@ -116,7 +116,7 @@ window.TRAVEL_DATA = {
       "lat": 34.76,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/河南郑州/河南郑州_01.jpg"
+        "assets/河南郑州/河南郑州_01.jpg"
       ],
       "concept": null
     },
@@ -127,15 +127,15 @@ window.TRAVEL_DATA = {
       "lat": 34.34,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/陕西西安/陕西西安_01.jpg",
-        "assets/photos/陕西西安/陕西西安_02.jpg",
-        "assets/photos/陕西西安/陕西西安_03.jpg",
-        "assets/photos/陕西西安/陕西西安_04.jpg",
-        "assets/photos/陕西西安/陕西西安_05.jpg",
-        "assets/photos/陕西西安/陕西西安_06.jpg",
-        "assets/photos/陕西西安/陕西西安_07.jpg",
-        "assets/photos/陕西西安/陕西西安_08.jpg",
-        "assets/photos/陕西西安/陕西西安_09.jpg"
+        "assets/陕西西安/陕西西安_01.jpg",
+        "assets/陕西西安/陕西西安_02.jpg",
+        "assets/陕西西安/陕西西安_03.jpg",
+        "assets/陕西西安/陕西西安_04.jpg",
+        "assets/陕西西安/陕西西安_05.jpg",
+        "assets/陕西西安/陕西西安_06.jpg",
+        "assets/陕西西安/陕西西安_07.jpg",
+        "assets/陕西西安/陕西西安_08.jpg",
+        "assets/陕西西安/陕西西安_09.jpg"
       ],
       "concept": null
     },
@@ -146,12 +146,12 @@ window.TRAVEL_DATA = {
       "lat": 30.57,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/四川成都/四川成都_01.jpg",
-        "assets/photos/四川成都/四川成都_02.jpg",
-        "assets/photos/四川成都/四川成都_03.jpg",
-        "assets/photos/四川成都/四川成都_04.jpg",
-        "assets/photos/四川成都/四川成都_05.jpg",
-        "assets/photos/四川成都/四川成都_06.jpg"
+        "assets/四川成都/四川成都_01.jpg",
+        "assets/四川成都/四川成都_02.jpg",
+        "assets/四川成都/四川成都_03.jpg",
+        "assets/四川成都/四川成都_04.jpg",
+        "assets/四川成都/四川成都_05.jpg",
+        "assets/四川成都/四川成都_06.jpg"
       ],
       "concept": null
     },
@@ -162,7 +162,7 @@ window.TRAVEL_DATA = {
       "lat": 31.13,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/四川德阳/四川德阳_01.jpg"
+        "assets/四川德阳/四川德阳_01.jpg"
       ],
       "concept": null
     },
@@ -173,23 +173,23 @@ window.TRAVEL_DATA = {
       "lat": 29.56,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/重庆/重庆_01.jpg",
-        "assets/photos/重庆/重庆_02.jpg",
-        "assets/photos/重庆/重庆_03.jpg",
-        "assets/photos/重庆/重庆_04.jpg",
-        "assets/photos/重庆/重庆_05.jpg",
-        "assets/photos/重庆/重庆_06.jpg",
-        "assets/photos/重庆/重庆_07.jpg",
-        "assets/photos/重庆/重庆_08.jpg",
-        "assets/photos/重庆/重庆_09.jpg",
-        "assets/photos/重庆/重庆_10.jpg",
-        "assets/photos/重庆/重庆_11.jpg",
-        "assets/photos/重庆/重庆_12.jpg",
-        "assets/photos/重庆/重庆_13.jpg",
-        "assets/photos/重庆/重庆_14.jpg",
-        "assets/photos/重庆/重庆_15.jpg",
-        "assets/photos/重庆/重庆_16.jpg",
-        "assets/photos/重庆/重庆_17.jpg"
+        "assets/重庆/重庆_01.jpg",
+        "assets/重庆/重庆_02.jpg",
+        "assets/重庆/重庆_03.jpg",
+        "assets/重庆/重庆_04.jpg",
+        "assets/重庆/重庆_05.jpg",
+        "assets/重庆/重庆_06.jpg",
+        "assets/重庆/重庆_07.jpg",
+        "assets/重庆/重庆_08.jpg",
+        "assets/重庆/重庆_09.jpg",
+        "assets/重庆/重庆_10.jpg",
+        "assets/重庆/重庆_11.jpg",
+        "assets/重庆/重庆_12.jpg",
+        "assets/重庆/重庆_13.jpg",
+        "assets/重庆/重庆_14.jpg",
+        "assets/重庆/重庆_15.jpg",
+        "assets/重庆/重庆_16.jpg",
+        "assets/重庆/重庆_17.jpg"
       ],
       "concept": null
     },
@@ -200,13 +200,13 @@ window.TRAVEL_DATA = {
       "lat": 28.23,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/湖南长沙/湖南长沙_01.jpg",
-        "assets/photos/湖南长沙/湖南长沙_02.jpg",
-        "assets/photos/湖南长沙/湖南长沙_03.jpg",
-        "assets/photos/湖南长沙/湖南长沙_04.jpg",
-        "assets/photos/湖南长沙/湖南长沙_05.jpg",
-        "assets/photos/湖南长沙/湖南长沙_06.jpg",
-        "assets/photos/湖南长沙/湖南长沙_07.jpg"
+        "assets/湖南长沙/湖南长沙_01.jpg",
+        "assets/湖南长沙/湖南长沙_02.jpg",
+        "assets/湖南长沙/湖南长沙_03.jpg",
+        "assets/湖南长沙/湖南长沙_04.jpg",
+        "assets/湖南长沙/湖南长沙_05.jpg",
+        "assets/湖南长沙/湖南长沙_06.jpg",
+        "assets/湖南长沙/湖南长沙_07.jpg"
       ],
       "concept": null
     },
@@ -217,11 +217,11 @@ window.TRAVEL_DATA = {
       "lat": 31.3,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/江苏苏州/江苏苏州_01.jpg",
-        "assets/photos/江苏苏州/江苏苏州_02.jpg",
-        "assets/photos/江苏苏州/江苏苏州_03.jpg",
-        "assets/photos/江苏苏州/江苏苏州_04.jpg",
-        "assets/photos/江苏苏州/江苏苏州_05.jpg"
+        "assets/江苏苏州/江苏苏州_01.jpg",
+        "assets/江苏苏州/江苏苏州_02.jpg",
+        "assets/江苏苏州/江苏苏州_03.jpg",
+        "assets/江苏苏州/江苏苏州_04.jpg",
+        "assets/江苏苏州/江苏苏州_05.jpg"
       ],
       "concept": null
     },
@@ -232,13 +232,13 @@ window.TRAVEL_DATA = {
       "lat": 30.52,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/湖北武汉/湖北武汉_01.jpg",
-        "assets/photos/湖北武汉/湖北武汉_02.jpg",
-        "assets/photos/湖北武汉/湖北武汉_03.jpg",
-        "assets/photos/湖北武汉/湖北武汉_04.jpg",
-        "assets/photos/湖北武汉/湖北武汉_05.jpg",
-        "assets/photos/湖北武汉/湖北武汉_06.jpg",
-        "assets/photos/湖北武汉/湖北武汉_07.jpg"
+        "assets/湖北武汉/湖北武汉_01.jpg",
+        "assets/湖北武汉/湖北武汉_02.jpg",
+        "assets/湖北武汉/湖北武汉_03.jpg",
+        "assets/湖北武汉/湖北武汉_04.jpg",
+        "assets/湖北武汉/湖北武汉_05.jpg",
+        "assets/湖北武汉/湖北武汉_06.jpg",
+        "assets/湖北武汉/湖北武汉_07.jpg"
       ],
       "concept": null
     },
@@ -249,9 +249,9 @@ window.TRAVEL_DATA = {
       "lat": 39.9,
       "hasPhotos": true,
       "photos": [
-        "assets/photos/北京/北京_01.jpg",
-        "assets/photos/北京/北京_02.jpg",
-        "assets/photos/北京/北京_03.jpg"
+        "assets/北京/北京_01.jpg",
+        "assets/北京/北京_02.jpg",
+        "assets/北京/北京_03.jpg"
       ],
       "concept": null
     },
